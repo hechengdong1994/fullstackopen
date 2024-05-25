d7b492dc792f66000de14585d12e50aed849377a
ChengDong He <569186076@qq.com>
2024-05-25 09:45:27 +0000

    add exercise phonebookbackend of part3a

---
+++ part3/phonebookbackend/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+const app = express()
+
+app.get('', (request, response) => {
+    response.end('Hello World')
+})
+app.get('/info', (request, response) => {
+    response
+        .contentType('text/html')
+        .end(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date().toISOString()} </p>`)
+})
+
+
+app.use(express.json())
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+
+    if (!person.name || !person.number) {
+        return response.status(400).json({ error: 'name and number are required' }).end()
+    }
+    if (persons.find(p => p.name === person.name)) {
+        return response.status(400).json({ error: 'name must be unique' }).end()
+    }
+
+    person.id = Math.floor(Math.random() * 100);
+
+    persons = persons.concat(person)
+
+    return response.status(201).json(person).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
