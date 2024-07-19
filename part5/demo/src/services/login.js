c64542ff3a2b05eb0352d306dec195548d953f2a
ChengDong He <569186076@qq.com>
2024-07-19 09:07:02 +0000

    make part5 no backend

--- part5/demo/src/services/login.js
+++ part5/demo/src/services/login.js
@@ -1,10 +1,11 @@
-import axios from "axios";
-import BACKEND_HOST from "./host";
-const baseUrl = `${BACKEND_HOST}/api/login`
+// import axios from "axios";
+// import BACKEND_HOST from "./host";
+// const baseUrl = `${BACKEND_HOST}/api/login`
 
 const login = async credentials => {
-    const response = await axios.post(baseUrl, credentials)
-    return response.data
+    // const response = await axios.post(baseUrl, credentials)
+    // return response.data
+    return {token:'111'}
 }
 
 export default { login }
--- part5/demo/src/services/notes.js
+++ part5/demo/src/services/notes.js
@@ -1,35 +1,50 @@
-import axios from "axios";
-import BACKEND_HOST from "./host";
-const baseUrl = `${BACKEND_HOST}/api/notes`
+// import BACKEND_HOST from "./host";
+// const baseUrl = `${BACKEND_HOST}/api/notes`
 
-let token = null
+// let token = null
 
 const setToken = newToken => {
-    token = `bearer ${newToken}`
+    // token = `bearer ${newToken}`
 }
 
-const getAll = () => {
-    const request = axios.get(baseUrl)
-    const nonExisting = {
+let blogs = [
+    {
         id: 10000,
         content: 'This note is not saved to server',
         date: '2019-05-30T17:30:31.098Z',
-        important: true,
+        important: true
     }
-    return request.then(response => response.data.concat(nonExisting))
+]
+
+const getAll = async () => {
+    // const request = axios.get(baseUrl)
+    // const nonExisting = {
+    //     id: 10000,
+    //     content: 'This note is not saved to server',
+    //     date: '2019-05-30T17:30:31.098Z',
+    //     important: true,
+    // }
+    // return request.then(response => response.data.concat(nonExisting))
+    return blogs
 }
 
 const create = async newObject => {
-    const config = {
-        headers: { Authorization: token }
-    }
-    const response = await axios.post(baseUrl, newObject, config)
-    return response.data
+    // const config = {
+    //     headers: { Authorization: token }
+    // }
+    // const response = await axios.post(baseUrl, newObject, config)
+    // return response.data
+    const data = { ...newObject, id: Date.now() }
+    blogs.concat(data)
+    return data
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
+    // const request = axios.put(`${baseUrl}/${id}`, newObject)
+    // return request.then(response => response.data)
+    const data = { ...newObject, id: id }
+    blogs = blogs.map(blog => blog.id === id ? data : blog)
+    return data
 }
 
 export default { getAll, create, update, setToken }
